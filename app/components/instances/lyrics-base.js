eac4a5183c0bbb965334538bd612c894f497fe73
picklefarmer <aporth2@gmail.com>
2017-01-25 17:19:11 -0500

    toggle-button 'reversable display text'

--- app/components/instances/lyrics-base.js
+++ app/components/instances/lyrics-base.js
@@ -1,5 +1,5 @@
 export default {
-	isStats:{"name":'Hide Labels',type:'toggle',"emoji":"\uD83D\uDE4B"},
+	isStats:{"name":['Show Labels','Hide Labels'],type:'toggle',"emoji":"\uD83D\uDE4B"},
 //  saveSelection: {"name": "Save","emoji":"\uD83D\uDE46"},
  };
 
--- app/components/toggle-button.js
+++ app/components/toggle-button.js
@@ -5,6 +5,14 @@ export default Ember.Component.extend({
 	song:Ember.inject.service(),
 	classNames:['squaredOne'],
 	classNameBindings:['bool:hit'],
+  displayText:Ember.computed('bool','desc',function(desc){
+    desc = this.get('desc')
+    if(typeof desc === 'object'){
+      return desc[~~this.get('bool')]
+    }else{
+      return desc
+    }
+  }),
   actions:{
     actionUp(){
       if(this.action){
